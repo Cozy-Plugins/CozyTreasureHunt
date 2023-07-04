1917754d420e43fb915cef4eca175758d871aefa
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-07-04 18:02:50 +0100

    Optimised

--- src/main/java/com/github/cozyplugins/cozytreasurehunt/storage/TreasureStorage.java
+++ src/main/java/com/github/cozyplugins/cozytreasurehunt/storage/TreasureStorage.java
@@ -4,7 +4,6 @@ import com.github.cozyplugins.cozylibrary.ConsoleManager;
 import com.github.cozyplugins.cozytreasurehunt.Treasure;
 import com.github.cozyplugins.cozytreasurehunt.storage.configuration.TreasureConfigurationDirectory;
 import com.github.smuddgge.squishyconfiguration.implementation.yaml.YamlConfiguration;
-import com.github.smuddgge.squishyconfiguration.interfaces.Configuration;
 import com.github.smuddgge.squishyconfiguration.interfaces.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
