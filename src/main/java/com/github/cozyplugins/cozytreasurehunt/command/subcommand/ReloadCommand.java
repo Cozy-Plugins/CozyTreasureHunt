a1f87bb630dbf2759881784098346f782d349a62
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-07-19 19:36:57 +0100

    Added reload command

---
+++ src/main/java/com/github/cozyplugins/cozytreasurehunt/command/subcommand/ReloadCommand.java
@@ -0,0 +1,72 @@
+package com.github.cozyplugins.cozytreasurehunt.command.subcommand;
+
+import com.github.cozyplugins.cozylibrary.CozyLibrary;
+import com.github.cozyplugins.cozylibrary.CozyPlugin;
+import com.github.cozyplugins.cozylibrary.command.command.CommandType;
+import com.github.cozyplugins.cozylibrary.command.datatype.CommandArguments;
+import com.github.cozyplugins.cozylibrary.command.datatype.CommandStatus;
+import com.github.cozyplugins.cozylibrary.command.datatype.CommandSuggestions;
+import com.github.cozyplugins.cozylibrary.command.datatype.CommandTypePool;
+import com.github.cozyplugins.cozylibrary.user.ConsoleUser;
+import com.github.cozyplugins.cozylibrary.user.FakeUser;
+import com.github.cozyplugins.cozylibrary.user.PlayerUser;
+import com.github.cozyplugins.cozylibrary.user.User;
+import com.github.cozyplugins.cozytreasurehunt.storage.ConfigFile;
+import com.github.cozyplugins.cozytreasurehunt.storage.TreasureStorage;
+import com.github.smuddgge.squishyconfiguration.interfaces.ConfigurationSection;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Represents the reload command.
+ * Reloads all configuration files.
+ */
+public class ReloadCommand implements CommandType {
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "reload";
+    }
+
+    @Override
+    public @Nullable String getSyntax() {
+        return "/[parent] [name]";
+    }
+
+    @Override
+    public @Nullable String getDescription() {
+        return "Used to reload the configuration.";
+    }
+
+    @Override
+    public @Nullable CommandTypePool getSubCommandTypes() {
+        return null;
+    }
+
+    @Override
+    public @Nullable CommandSuggestions getSuggestions(@NotNull User user, @NotNull ConfigurationSection section, @NotNull CommandArguments arguments) {
+        return null;
+    }
+
+    @Override
+    public @Nullable CommandStatus onUser(@NotNull User user, @NotNull ConfigurationSection section, @NotNull CommandArguments arguments) {
+        ConfigFile.setup();
+        user.sendMessage("&7Configuration reloaded.");
+        return new CommandStatus();
+    }
+
+    @Override
+    public @Nullable CommandStatus onPlayer(@NotNull PlayerUser user, @NotNull ConfigurationSection section, @NotNull CommandArguments arguments) {
+        return null;
+    }
+
+    @Override
+    public @Nullable CommandStatus onFakeUser(@NotNull FakeUser user, @NotNull ConfigurationSection section, @NotNull CommandArguments arguments) {
+        return null;
+    }
+
+    @Override
+    public @Nullable CommandStatus onConsole(@NotNull ConsoleUser user, @NotNull ConfigurationSection section, @NotNull CommandArguments arguments) {
+        return null;
+    }
+}
