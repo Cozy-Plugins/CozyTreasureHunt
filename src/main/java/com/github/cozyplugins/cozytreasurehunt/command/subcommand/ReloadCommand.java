f08d43daad7dd5ca5794d3aa5aefb0e4da92c641
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-07-19 19:52:31 +0100

    Optimise

--- src/main/java/com/github/cozyplugins/cozytreasurehunt/command/subcommand/ReloadCommand.java
+++ src/main/java/com/github/cozyplugins/cozytreasurehunt/command/subcommand/ReloadCommand.java
@@ -1,7 +1,5 @@
 package com.github.cozyplugins.cozytreasurehunt.command.subcommand;
 
-import com.github.cozyplugins.cozylibrary.CozyLibrary;
-import com.github.cozyplugins.cozylibrary.CozyPlugin;
 import com.github.cozyplugins.cozylibrary.command.command.CommandType;
 import com.github.cozyplugins.cozylibrary.command.datatype.CommandArguments;
 import com.github.cozyplugins.cozylibrary.command.datatype.CommandStatus;
@@ -12,7 +10,6 @@ import com.github.cozyplugins.cozylibrary.user.FakeUser;
 import com.github.cozyplugins.cozylibrary.user.PlayerUser;
 import com.github.cozyplugins.cozylibrary.user.User;
 import com.github.cozyplugins.cozytreasurehunt.storage.ConfigFile;
-import com.github.cozyplugins.cozytreasurehunt.storage.TreasureStorage;
 import com.github.smuddgge.squishyconfiguration.interfaces.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
